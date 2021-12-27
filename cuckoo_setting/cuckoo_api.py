313e3f9e93649c7004f9e32fed95fd45dacc74dc
JINBEOMJIN <jinbumjin@naver.com>
2021-12-27 12:50:09 +0900

    update

--- cuckoo_setting/cuckoo_api.py
+++ cuckoo_setting/cuckoo_api.py
@@ -6,9 +6,9 @@ import subprocess as sp
 # port range (8090~8094)
 num =5 
 def cuckoo_api(n):
-    os.system("/data/beomjin/cuckoo/cuckoovenv/bin/cuckoo --cwd /data/beomjin/cuckoo/conf_path/cuckoo"+str(n)+" api"+" -p "+str(8089+n))
+    os.system("cuckoo --cwd path/cuckoo_conf"+str(n)+" api"+" -p "+str(8089+n))
 
 if __name__ == "__main__":
     with Pool(num) as p:
-        p.map(cuckoo_api, [i for i in range(2,num+2)])
+        p.map(cuckoo_api, [i for i in range(1,num+1)])
 
--- cuckoo_setting/cuckoo_debug.py
+++ cuckoo_setting/cuckoo_debug.py
@@ -6,9 +6,9 @@ import subprocess as sp
 num=5
 
 def cuckoo_debug(n):
-    os.system("/data/beomjin/cuckoo/cuckoovenv/bin/cuckoo --cwd /data/beomjin/cuckoo/conf_path/cuckoo"+str(n))
+    os.system("cuckoo --cwd path/cuckoo_conf"+str(n))
 
 if __name__ == "__main__":
     with Pool(num) as p:
-        p.map(cuckoo_debug, [i for i in range(2,num+2)])
+        p.map(cuckoo_debug, [i for i in range(1,num+1)])
 
